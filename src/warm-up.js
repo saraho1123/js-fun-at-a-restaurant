26bd2826cc1155bfbaefe379ab1e6d6b899a4e5e
Sarah Osgood <sarahjs117@gmail.com>
2020-07-05 21:44:42 -0500

    Completed warmup

--- src/warm-up.js
+++ src/warm-up.js
@@ -5,34 +5,60 @@
 
 //------------------------- Variables -------------------------//
 
+// Node src/warm-up.js 
+
 
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10
+
+console.log(food);
+console.log(type);
+// console.log(inStock);
 
 
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
 
+// inStock = 10 * 2
 
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// console.log(inStock);
+
+
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
+
+// inStock = (10 * 2) - 1
+
+// console.log(inStock);
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+// var isFavorite = false
+//
+// console.log(isFavorite);
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+function whenToOrderMore() {
+  if (inStock < 20) {
+    return (shouldOrderMore = true);
+  } else {
+    return (shouldOrderMore = false);
+  }
+}
+
+var shouldOrderMore = whenToOrderMore();
+
+console.log(shouldOrderMore);
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,8 +66,17 @@
 
 
 // Express the following in code: If the variable "stockCount" is less than 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
+
+var stockCount = 0
+  if (stockCount < 1) {
+    console.log(`I'm going to eat some ${food}.`);
+  } else {
+    console.log(`We need to get some more ${food}.`);
+  }
+
+console.log(stockCount);
 
 
 
@@ -50,7 +85,16 @@
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
+var isFavorite = true
 
+if (isFavorite == true) {
+  console.log(`My favorite food is ${food}`);
+} else {
+  console.log(`I recently ate ${food}`);
+}
+
+
+console.log(isFavorite);
 
 //------------------------- Arrays -------------------------//
 
@@ -58,16 +102,22 @@
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
 
+var vegetables = ["carrots", "avacados", "tomatos", "kale", "spinach"]
+
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+console.log(vegetables[2]);
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
 
+var fruits = ["strawberries", "blueberries", "oranges", "bananas", "cherries", "durian", "kumquats"];
 
 // Write the code to access the 5th element of the array, using bracket notation.
+
+console.log(fruits[4]);
 
 
 //------------------------- Objects -------------------------//
@@ -76,7 +126,17 @@
 // keys: name, and isOpen. The value for name should the string 'King Soopers', and
 // the value for isOpen should be true
 
+var groceryStore = {
+  name: "King Soopers",
+  isOpen: true
+}
+
+console.log(groceryStore);
 
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
-// notation to do this. DO NOT modify your code from above.
+// notation to do this. DO NOT modify your code from above
+
+groceryStore.location = "Denver, Colorado";
+
+console.log(groceryStore);
