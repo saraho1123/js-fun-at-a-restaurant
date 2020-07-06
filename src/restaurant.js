5460f57f83b544c015345df05f94599eab1bfa0a
Sarah Osgood <sarahjs117@gmail.com>
2020-07-05 21:46:56 -0500

    Almost completed restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,30 @@
+function createRestaurant(restaurant) {
+  return {
+    name: restaurant,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+}
+
+function addMenuItem(restaurant, lunchKey) {
+  return restaurant.menus[lunchKey.type].push(lunchKey);
+
+}
+
+
+// if (ingredientArr.includes(ingredient1) == false) {
+//   ingredientArr.push(ingredient1);
+
+// Worked in a group on this one. We used both bracket and dot notation
+// I understand this working, but I am fairly certain I can't duplicate it.
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
